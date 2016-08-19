ff4b4d0620eacd8847ee917f08d196a83163769b
Andy <andy952737@hotmail.com>
2016-08-19 17:24:59 +0800

    8/19

--- js/jquery-js-demo.js
+++ js/jquery-js-demo.js
@@ -3,3 +3,15 @@ $(document).ready(function(){
         alert("Value: " + $("#js-p-demo").text());
     });
 });
+
+// Ajax Load jQuery
+$(document).ready(function(){
+    $("button").click(function(){
+        $("#div1").load("demo_test.txt", function(responseTxt, statusTxt, xhr){
+            if(statusTxt == "success")
+                alert("External content loaded successfully!");
+            if(statusTxt == "error")
+                alert("Error: " + xhr.status + ": " + xhr.statusText);
+        });
+    });
+});
